409f2dcca7539bb4ba71acd955b7b596eaeb9dfe
Luke Raimondo Moia <pe75yel@eduvaud.ch>
2023-09-01 14:25:49 +0200

    Menu bougeable

--- SpacyInvaders/Program.cs
+++ SpacyInvaders/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace SpacyInvaders
 {
@@ -20,7 +21,11 @@ namespace SpacyInvaders
 
     class jeux
     {
+        Timer timTimerJeux = new Timer();
         int intLangue { get; set; }
+        string strPoints = "";
+        string strFPS = "FPS: ";
+        int intPoints = 0;
         public void AffichageMenuLangue()
         {
             Console.Write("Language (0=fr, 1=en) : ");
@@ -39,14 +44,12 @@ namespace SpacyInvaders
             }
         }
 
-        public void AffichageMenuPourJouer()
+        private void AffichageMenuPourJouer()
         {
             string strPlay = "";
             string strOption = "";
             string strResult = "";
             string strQuit = "";
-            string strPoints = "";
-            string strFPS = "FPS: ";
 
             if (intLangue == 0)
             {
@@ -66,7 +69,82 @@ namespace SpacyInvaders
             }
 
             Console.WriteLine ($"SpicyInvader II\n---------------\n\n{strPlay}\n{strOption}\n{strResult}\n{strQuit}");
-            Console.ReadLine();
+            // méthode pour pouvoir faire la boucle
+            BoucleMenu();
+        }
+
+        private void BoucleMenu()
+        {
+            ConsoleKey cskConsole = ConsoleKey.A;
+            const int intMax = 6;
+            const int intMin = 3;
+            int x = intMin;
+
+            /*Console.CursorVisible = false;
+            Console.BackgroundColor = ConsoleColor.Red();*/
+            for (; cskConsole != ConsoleKey.Enter;)
+            {
+                Console.SetCursorPosition(0,x);
+                cskConsole = Console.ReadKey().Key;
+                if (cskConsole == ConsoleKey.DownArrow || cskConsole == ConsoleKey.S)
+                {
+                    if (x != intMax)
+                    {
+                        x++;
+                    }
+                    else
+                    {
+                        x = intMin;
+                    }
+                }
+                else if (cskConsole == ConsoleKey.UpArrow || cskConsole == ConsoleKey.W)
+                {
+                    if (x != intMin)
+                    {
+                        x--;
+                    }
+                    else
+                    {
+                        x = intMax; 
+                    }
+                }
+            }
+            if(x - intMin == 0)
+            {
+                // méthode pour lancer le jeu
+                LancerLeJeu();
+                for (;;)
+                {
+
+                }
+            }
+            else if (x - intMin == 1)
+            {
+                // méthode pour les options
+                
+            }
+            else if (x - intMin == 2)
+            {
+                // méthode pour les résultats
+                
+            }
+        }
+
+        private void LancerLeJeu()
+        {
+            timTimerJeux.Enabled = true;
+            timTimerJeux.AutoReset = true;
+            timTimerJeux.Interval = 600;
+            timTimerJeux.Start();
+            timTimerJeux.Elapsed += Jeux;
+        }
+        private void Jeux(object valeur, System.Timers.ElapsedEventArgs Temps)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(Console.BufferWidth - strPoints.Length - 1, 0);
+            Console.Write(strPoints + intPoints);
+            Console.SetCursorPosition(Console.BufferWidth - strPoints.Length - 1, 1);
+            Console.Write(strFPS + intLangue);
         }
     }
 }
